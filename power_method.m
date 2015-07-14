3e518c0f842fb70d734672002017285a012dd17d
Filip Moons <fmoons@vub.ac.be>
2015-07-14 11:21:17 +0200

    14/7/2015

--- power_method.m
+++ power_method.m
@@ -1,54 +1,19 @@
-function [ y, lambda, it_num ] = power_method ( n, a, y, it_max, tol )
-
-%  Force Y to be a column vector.
-  y = y ( : );
-  lambda = 0;
-
-%  Force Y to be a vector of unit norm.
-
-  y = y / norm ( y );
-
-  it_num = 0;
-
-  y_old = y;
-
-  ay = a * y;
-  lambda = y' * ay;
-  y = ay / norm ( ay );
-  if ( lambda < 0.0 )
-    y = - y;
-  end
-
-  val_dif = 0.0;
-
-  cos_y1y2 = y' * y_old;
-  sin_y1y2 = sqrt ( ( 1.0 - cos_y1y2 ) * ( 1.0 + cos_y1y2 ) ); 
-
-  for it_num = 1 : it_max
-
-    lambda_old = lambda;
-    y_old = y;
-
-    ay = a * y;
-    lambda = y' * ay;
-    y = ay / norm ( ay );
-    if ( lambda < 0.0 )
-      y = - y;
+function [y, mu] = power_metho(A, y, TOL)
+k = 1;
+mu = 0;
+while true
+    z = A*y;
+    mu_previous = mu;
+    mu = norm(z,2);
+    y_previous = y;
+    y = z/mu;
+    k = k + 1;
+    if and(k>2,abs(mu - mu_previous)<TOL)
+        break;
     end
-
-    val_dif = abs ( lambda - lambda_old );
-
-    sin_y1y2 = 0;
-    cos_y1y2 = y' * y_old;
-    sin_y1y2 = sqrt ( ( 1.0 - cos_y1y2 ) * ( 1.0 + cos_y1y2 ) );
-
-    if ( val_dif <= tol )
-      break
-    end 
-
-  end
-
-  y = ay / lambda;
-
-  return
+end
+if (y(1) == -y_previous(1))
+    mu = -mu;
+end
+return;
 end
